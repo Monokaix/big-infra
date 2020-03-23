5f84ff0ef2b13cc9bcefea93ec618f2951fdd50d
Monokai <2536818783@qq.com>
2020-03-23 16:51:44 +0800

    update

--- go.mod
+++
@@ -1,3 +0,0 @@
-module big-infra
-
-go 1.12
--- pkg/apiserver/cmd/apiserver.go
+++ pkg/apiserver/cmd/apiserver.go
@@ -1,1 +1,36 @@
-package cmd
+package main
+
+import (
+	"os"
+
+	"big-infra/pkg/apiserver/common"
+	"big-infra/pkg/apiserver/config"
+	"big-infra/pkg/apiserver/service"
+
+	logger "github.com/sirupsen/logrus"
+)
+
+func main() {
+	logger.Info()
+	logger.Info("starting apiserver for TuPam")
+
+	confPath := os.Getenv("API_SRV_CONF_PATH")
+	if confPath == "" {
+		// dev mode, configure path load from file
+		logger.Info("load configure from file")
+		confPath = common.CONF_PATH
+	}
+
+	env, err := config.Init(confPath)
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	s := service.New(env)
+
+	go s.SignalHandler()
+
+	if err := s.Start(env.Cfg.GrpcSrv.Address); err != nil {
+		logger.Panic(err)
+	}
+}
